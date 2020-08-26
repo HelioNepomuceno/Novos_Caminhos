757769b0ef8cf0a1c7e2289e80cd4051f95fe54f
Helio Regis Frota Nepomuceno <heliofrotacontabil@gmail.com>
2020-08-26 04:09:11 -0300

    first commit

---
+++ 01 vscode/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+/* TODO REVISAR CÁLCULO DE AMORTIZAÇÃO */
+/* FIXME CORRIGIR CÁLCULO DAS MÉDIAS DOS ALUNOS */
+
+/**@author Helio <heliofrotacontabil@gmail.com> */
+phpinfo();
+
+/**
+ * Class constructor.
+ */
